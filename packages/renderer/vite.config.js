a599f52d0fa0cb57c8d155bb062ed7dbf9d793fc
Federico Vázquez <federico.vazquez@mercadolibre.com>
2022-01-07 01:58:34 -0300

    Switch to React from Vue

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 
-import {chrome} from '../../.electron-vendors.cache.json';
-import {join} from 'path';
-import {builtinModules} from 'module';
-import vue from '@vitejs/plugin-vue';
+import { chrome } from "../../.electron-vendors.cache.json";
+import { join } from "path";
+import { builtinModules } from "module";
+import react from "@vitejs/plugin-react";
 
 const PACKAGE_ROOT = __dirname;
 
@@ -16,11 +16,11 @@ const config = {
   root: PACKAGE_ROOT,
   resolve: {
     alias: {
-      '/@/': join(PACKAGE_ROOT, 'src') + '/',
+      "/@/": join(PACKAGE_ROOT, "src") + "/",
     },
   },
-  plugins: [vue()],
-  base: '',
+  plugins: [react()],
+  base: "./",
   server: {
     fs: {
       strict: true,
@@ -29,12 +29,10 @@ const config = {
   build: {
     sourcemap: true,
     target: `chrome${chrome}`,
-    outDir: 'dist',
-    assetsDir: '.',
+    outDir: "dist",
+    assetsDir: ".",
     rollupOptions: {
-      external: [
-        ...builtinModules,
-      ],
+      external: [...builtinModules],
     },
     emptyOutDir: true,
     brotliSize: false,
